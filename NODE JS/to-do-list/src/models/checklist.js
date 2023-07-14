937b4201f92c98b86f6704cecb1e6849081c6a7d
Vinicius <viniciuspereiraa51@gmail.com>
2023-07-14 14:11:32 -0300

    Practice Javascript OneBitCode

--- NODE JS/to-do-list/src/models/checklist.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const checklistSchema = mongoose.Schema({
-  name: { type: String, required: true },
-  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }], //1 checklist para varias tarefas
-}); //modelo
-
-module.exports = mongoose.model("Checklist", checklistSchema);
