aa19a34237bcf70a85ee49ffc944fbd954edff35
Vinicius Pereirx <viniciuspereiraa51@gmail.com>
2023-10-30 13:35:08 -0300

    Practice Javascript OneBitCode

---
+++ REACT JS/03-componentes/index.jsx
@@ -0,0 +1,14 @@
+function Button() {
+  return <button>Meu botão</button>;
+}
+
+function Container() {
+  return (
+    <div className="container">
+      <header>
+        {" "}
+        <h1></h1>
+      </header>
+    </div>
+  );
+}
