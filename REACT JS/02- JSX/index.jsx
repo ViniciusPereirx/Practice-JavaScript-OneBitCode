0b6dd0131ea76ce70c03ec827aed0e9f7866b65c
Vinicius Pereirx <viniciuspereiraa51@gmail.com>
2023-10-30 02:46:40 -0300

    Practice Javascript OneBitCode

---
+++ REACT JS/02- JSX/index.jsx
@@ -0,0 +1,16 @@
+const element = <h1>Olá, mundo</h1>;
+
+const anotherElement = (
+  <div>
+    <h1>Texto</h1>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+    <h2></h2>
+  </div>
+);
